6c6eff094a61909a49e282dafe460b994e942c07
felipeauroux <felipe.rrezend@gmail.com>
2023-02-27 20:02:14 -0300

    Trabalho01Mobile

--- app/src/main/java/com/example/consultordecerveja_feliperrezende/MainActivity.java
+++ app/src/main/java/com/example/consultordecerveja_feliperrezende/MainActivity.java
@@ -17,7 +17,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-Spinner tipagem;
+    Spinner tipagem;
+    String item;
+    RecyclerView recyclerView;
+    List<listagem> marcaspilsen;
+    List<listagem> marcasbock;
+    List<listagem> marcaswitbier;
+    List<listagem> marcasweissbier;
+    List<listagem> marcasamericanipa;
+    List<listagem> marcasstout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,32 +37,104 @@ Spinner tipagem;
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.tiposCerveja, android.R.layout.simple_spinner_item);
         tipagem.setAdapter(adapter);
         Button Pesquisar = (Button) findViewById(R.id.pesquisa);
-        Pesquisar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String item = tipagem.getSelectedItem().toString();
-                //Alterar para receber o valor do spinner = switch/case?
-                Toast.makeText(getApplicationContext(), "Tipo escolhido: " + item, Toast.LENGTH_SHORT).show();
+    }
 
-               }
+    public void onClickBuscar(View view) {
+        item = tipagem.getSelectedItem().toString();
+        //Alterar para receber o valor do spinner = switch/case?
+        Toast.makeText(getApplicationContext(), "Tipo escolhido: " + item, Toast.LENGTH_SHORT).show();
 
-        });
+        recyclerView = findViewById(R.id.recyclerview);
+        marcaspilsen = new ArrayList<listagem>();
+        marcasbock = new ArrayList<listagem>();
+        marcaswitbier = new ArrayList<listagem>();
+        marcasweissbier = new ArrayList<listagem>();
+        marcasamericanipa = new ArrayList<listagem>();
+        marcasstout = new ArrayList<listagem>();
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        List<listagem> items = new ArrayList<listagem>();
-                items.add(new listagem("Artesanal Imigração",R.drawable.artesanalimigracaopilsen));
-        items.add(new listagem("Bamberg",R.drawable.bambergpilsen));
-        items.add(new listagem("Dama Bier",R.drawable.damabierpilsen));
-        items.add(new listagem("Eisenbahn",R.drawable.eisenbahnpilsen));
-        items.add(new listagem("Fred Bier",R.drawable.fredbierpilsen));
-        items.add(new listagem("Império",R.drawable.imperiopilsen));
-        items.add(new listagem("Patagonia",R.drawable.patagoniapilsen));
-        items.add(new listagem("Pilsner Urquell",R.drawable.pilsnerurquellpilsen));
-        items.add(new listagem("Schornstein",R.drawable.schornsteinpilsen));
-        items.add(new listagem("Wäls",R.drawable.walspevepilsen));
+    switch(item) {
+        case "Pilsen":
+            marcaspilsen.add(new listagem("Artesanal Imigração", R.drawable.artesanalimigracaopilsen));
+            marcaspilsen.add(new listagem("Bamberg", R.drawable.bambergpilsen));
+            marcaspilsen.add(new listagem("Dama Bier", R.drawable.damabierpilsen));
+            marcaspilsen.add(new listagem("Eisenbahn", R.drawable.eisenbahnpilsen));
+            marcaspilsen.add(new listagem("Fred Bier", R.drawable.fredbierpilsen));
+            marcaspilsen.add(new listagem("Império", R.drawable.imperiopilsen));
+            marcaspilsen.add(new listagem("Patagonia", R.drawable.patagoniapilsen));
+            marcaspilsen.add(new listagem("Pilsner Urquell", R.drawable.pilsnerurquellpilsen));
+            marcaspilsen.add(new listagem("Schornstein", R.drawable.schornsteinpilsen));
+            marcaspilsen.add(new listagem("Wäls", R.drawable.walspevepilsen));
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new MyAdapter(getApplicationContext(),items));
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MyAdapter(getApplicationContext(), marcaspilsen));
+            break;
+
+        case "Bock":
+            marcasbock.add(new listagem("Paulaner Salvator",R.drawable.paulanersalvatorbock));
+            marcasbock.add(new listagem("German Dunkler Weizenbock",R.drawable.germandunklerbock));
+            marcasbock.add(new listagem("Weizenbock Weihenstephaner Vitus",R.drawable.weizenbock));
+            marcasbock.add(new listagem("Schneider Weiss",R.drawable.schneiderweissbock));
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MyAdapter(getApplicationContext(),marcasbock));
+            break;
+        case "Witbier":
+            marcaswitbier.add(new listagem("Bier Hoff",R.drawable.bierhoffwitbier));
+            marcaswitbier.add(new listagem("Grand Cru",R.drawable.gradcruwitbier));
+            marcaswitbier.add(new listagem("Damm Estrella Inedit",R.drawable.dammwitbier));
+            marcaswitbier.add(new listagem("Dieu du Ciel",R.drawable.dieuducielwitbier));
+            marcaswitbier.add(new listagem("Hoegaarden",R.drawable.hoegaardenwitbier));
+            marcaswitbier.add(new listagem("La Trappe",R.drawable.latrappewitbier));
+            marcaswitbier.add(new listagem("Vedett",R.drawable.vedettwitbier));
+            marcaswitbier.add(new listagem("Baden Baden",R.drawable.badenbadenwitbier));
+            marcaswitbier.add(new listagem("Bastards",R.drawable.bastardswitbier));
+            marcaswitbier.add(new listagem("Faxe",R.drawable.faxewitbier));
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MyAdapter(getApplicationContext(),marcaswitbier));
+            break;
+
+        case "Weissbier":
+            marcasweissbier.add(new listagem("Hoegaarden",R.drawable.hoegaardenweissbier));
+            marcasweissbier.add(new listagem("Weihenstephaner Hefe",R.drawable.weihenstephanerweissbier));
+            marcasweissbier.add(new listagem("Patagonia",R.drawable.patagoniaweissbier));
+            marcasweissbier.add(new listagem("Eisenbahn",R.drawable.eisenbahnweissbier));
+            marcasweissbier.add(new listagem("Baden Baden",R.drawable.badenbadenweissbier));
+            marcasweissbier.add(new listagem("Vedett",R.drawable.vedettweissbier));
+            marcasweissbier.add(new listagem("Franziskaner Hefe",R.drawable.franziskanerweissbier));
+            marcasweissbier.add(new listagem("Erdinger Dunkel",R.drawable.erdingerweissbier));
+            marcasweissbier.add(new listagem("Erdinger Pikantus",R.drawable.erdingerpikantus));
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MyAdapter(getApplicationContext(),marcasweissbier));
+            break;
+
+        case "American IPA":
+            marcasamericanipa.add(new listagem("Colorado",R.drawable.coloradoipa));
+            marcasamericanipa.add(new listagem("Baden Baden",R.drawable.badenbadenipa));
+            marcasamericanipa.add(new listagem("Roleta Russa IPA",R.drawable.roletarussaipa));
+            marcasamericanipa.add(new listagem("Session Citra",R.drawable.sessioncitraipa));
+            marcasamericanipa.add(new listagem("Schornstein",R.drawable.schornsteinipa));
+            marcasamericanipa.add(new listagem("Goose",R.drawable.gooseipa));
+            marcasamericanipa.add(new listagem("Patagonia",R.drawable.patagoniaipa));
+            marcasamericanipa.add(new listagem("Bodebrown",R.drawable.bodebrownipa));
+            marcasamericanipa.add(new listagem("Tarantino",R.drawable.tarantinoipa));
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MyAdapter(getApplicationContext(),marcasamericanipa));
+            break;
+
+        case "Stout":
+            marcasstout.add(new listagem("Wäls Petroleum",R.drawable.walsstout));
+            marcasstout.add(new listagem("Leopoldina",R.drawable.leopoldinastout));
+            marcasstout.add(new listagem("Belhaven",R.drawable.belhavenstout));
+            marcasstout.add(new listagem("Fuller's",R.drawable.fullersstout));
+            marcasstout.add(new listagem("Schornstein",R.drawable.schornsteinstout));
+            marcasstout.add(new listagem("Dama Bier",R.drawable.damabierstout));
+            marcasstout.add(new listagem("Guinness",R.drawable.guinnessstout));
+            marcasstout.add(new listagem("Dádiva",R.drawable.dadivastout));
+            marcasstout.add(new listagem("Blumenau",R.drawable.blumenaustout));
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MyAdapter(getApplicationContext(),marcasstout));
+            break;
+
+        }
 
     }
 }
